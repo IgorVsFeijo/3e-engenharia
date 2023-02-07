081ba255bf5ff7a075bbf0f5ac0ac212842160bb
AprendIgor <107777408+IgorVsFeijo@users.noreply.github.com>
2023-02-07 00:00:46 -0300

    pagina refeita e estruturada
    paginas separadas por pastas
    modelo de estilo unico para todas as paginas
    criacao de um estilo de referencia para boxs, para que haja uma padronização nas mesmas
    criacao de um html base de onde vem a estrutura das boxes

---
+++ scripts/base.js
@@ -0,0 +1,82 @@
+//Top-bar
+
+//Menu Hamburguer
+let menu = document.getElementsByTagName('menu')[0]
+let header = document.getElementsByTagName('header')[0]
+let main = document.getElementsByTagName('main')[0]
+let topbar = document.getElementsByClassName('top-bar')[0]
+
+//Adiciona o icone do mapa aos icones da direita quando a tela se torna menor que 750px
+
+
+let end = document.getElementById("map")
+let rightSide  = document.getElementsByClassName('right')[0]
+let backup = rightSide.innerHTML
+
+let a = '<a href="https://goo.gl/maps/2m59dw9WSFD6tpeD8">     <img src="../../assets/imgs/maps-pin-black-icon.svg" id="pin" alt="Icon Endereço"></img></a>'
+
+mediaQueries()
+showMenu()
+
+function mediaQueries(){
+        
+    console.log(rightSide.children.length)
+
+    if(document.body.clientWidth < 750){
+    
+        if(rightSide.children.length <= 3){
+            rightSide.innerHTML += a
+            end.style.display = 'none'
+        }
+        
+    }
+
+    else{
+        if(rightSide.children.length > 3){
+            console.log(rightSide.innerHTML)
+            rightSide.innerHTML = backup
+
+            end.style.display = 'flex'
+        }
+    }
+    
+    //Menu Hamburguer
+
+    if(document.body.clientWidth < 645){
+        logo = document.getElementById('logo')
+        logo.src = "../assets/imgs/Logo-sem-legenda-100.png"
+        header.parentElement.insertBefore(menu, main)
+    }
+    else{
+        logo.src = "../assets/imgs/logo-3eengenharia-300.png"
+        header.appendChild(menu)
+    }
+
+    if(document.body.clientWidth < 450){
+        document.body.appendChild(topbar)
+    }
+
+    else{
+        document.body.insertBefore(topbar, header)
+    }
+
+}
+
+function showMenu(){
+    console.log(menu.classList[0])
+
+    if(menu.classList[0] == 'flex' && menu.parentElement != header){
+        menu.classList.remove('flex')
+    }
+    else{
+        menu.classList.add('flex')
+    }
+    
+}
+
+document.body.onresize = mediaQueries
+
+
+
+
+    
